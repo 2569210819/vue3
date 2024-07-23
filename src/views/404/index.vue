ea739fe3aed0d9d76f135187073880c7288f21b6
wanggang <17521791126@163.com>
2024-07-23 11:20:51 +0800

    test

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,6 +1,18 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: wanggang
+ * @Date: 2024-04-24 17:04:12
+ * @LastEditors: wanggang
+ * @LastEditTime: 2024-04-25 15:34:32
+-->
 <template>
   <div>404页面</div>
 </template>
-<script setup></script>
+<script setup>
+const ajax = new Promise((a, b) => {
+  a('success')
+})
+</script>
 
 <style lang="scss" scoped></style>
